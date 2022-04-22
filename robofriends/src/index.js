2baf7fbbe47122f5b7363b6f995e4dbf0c038295
Daphne Avril <daphneavril@outlook.com>
2022-04-22 10:31:43 +0200

    merge

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import {createRoot}  from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import App from './App'
 
 
 const root = createRoot(document.getElementById("root"))
 
 root.render(
-  <Hello greeting={'Hello' + 'React Ninja'}/>
+ <App/>  
 );
 
 // If you want to start measuring performance in your app, pass a function
